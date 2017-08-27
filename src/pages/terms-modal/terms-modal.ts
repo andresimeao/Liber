d8e9b090c98818dfeb30a96abc7b4d27fcdd1bfa
Dayane Rodrigues <day_rodrisantos@hotmail.com>
2017-08-27 14:48:41 -0300

    criacao do componente termModal

---
+++ src/pages/terms-modal/terms-modal.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the TermsModalPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-terms-modal',
+  templateUrl: 'terms-modal.html',
+})
+export class TermsModalPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad TermsModalPage');
+  }
+
+}
