f06c1b777405c079e0ab063013924ea18db99e05
andresimeao <andresimeao@yahoo.com.br>
2017-08-27 16:07:27 -0300

    cadastro de livro quase pronto

--- src/pages/create-book/create-book.ts
+++ src/pages/create-book/create-book.ts
@@ -13,19 +13,19 @@ import {TermsModalPage} from '../terms-modal/terms-modal';
 })
 export class CreateBookPage {
   
-  book = {
-    title:null,
-    synopsis:null,
-    category:null,
-    gender:null,
-    warnings:null
-  }
+  // book = {
+  //   title:null,
+  //   synopsis:null,
+  //   category:null,
+  //   gender:null,
+  //   warnings:null
+  // }
     
   addBookForm:FormGroup;
   constructor(public navCtrl: NavController, public navParams: NavParams,
   public afDB: AngularFireDatabase, public afAuth: AngularFireAuth, public loadingCtrl: LoadingController, private alertCtrl: AlertController,
   public formBuilder: FormBuilder, public modalCtrl: ModalController) {
-
+   
     this.addBookForm = this.formBuilder.group({
       title:['',Validators.compose([Validators.required])],
       synopsis:['', Validators.compose([Validators.required, Validators.minLength(6)])],
@@ -37,7 +37,13 @@ export class CreateBookPage {
   }
   
   public createBook(){
-    let Book = this.addBookForm.value;
+    let user = this.afAuth.auth.currentUser;
+    let book = this.addBookForm.value;
+    let bookDataBase = this.afDB.list('books');
+
+    bookDataBase.push({title:book.title, synopsis:book.synopsis, category:book.category, 
+    warnings:book.warnings, userId:user.uid});
+
   }
 
   openTermsModal() {
